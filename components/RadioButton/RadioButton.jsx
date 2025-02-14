1f300902dffc035fbe8e0fef7475e7e7d5be517a
Jimena Hernández <her21199@uvg.edu.gt>
2025-02-13 19:51:59 -0600

    update review form

--- components/RadioButton/RadioButton.jsx
+++ components/RadioButton/RadioButton.jsx
@@ -1,15 +1,15 @@
 "use client";
 
-const RadioButton = ({ value, onChange, star, checked }) => {
+const RadioButton = ({ value, onChange, star, checked, name}) => {
   return (
     <div className={`${star ? 'flex px-2' : 'flex flex-col w-full py-2'}`}>
-      <div className='flex flex-row w-full justify-start items-center gap-4'>
-        <label className={`${star? "hidden" : "font-futura text-sm text-black font-light"}`}>{value}</label>
+      <div className='flex flex-row w-full justify-between items-center gap-4'>
+        <label className={`${star ? "hidden" : "font-futura text-sm text-black font-light"}`}>{value}</label>
         {!star && (
           <input
             type="radio"
-            className={`radio checked:bg-secondary checked:border-secondary border-2 border-secondary/60`}
-            name="reason"
+            className={`radio checked:bg-secondary checked:border-secondary border-2 border-secondary/60 hover:bg-secondary/60`}
+            name={name}
             id={value}
             value={value}
             onChange={onChange}
@@ -18,10 +18,11 @@ const RadioButton = ({ value, onChange, star, checked }) => {
         {star && (
           <input
             type="radio"
-            name="rating"
-            className={`mask mask-star-2  hover:bg-primary ${checked ? 'bg-primary' : 'bg-primary/60'}`}
+            name={name}
+            className={`mask mask-star-2 hover:bg-primary ${checked ? 'bg-primary' : 'bg-primary/60'}`}
             value={value}
             onChange={onChange}
+            onMouseEnter={onChange}
           />
         )}
       </div>
--- components/ReviewForm/ReviewForm.jsx
+++ components/ReviewForm/ReviewForm.jsx
@@ -4,9 +4,9 @@ import RadioButton from '../RadioButton/RadioButton';
 import useRadioGroup from '../../hooks/useRadioGroup';
 import { useTranslation } from 'react-i18next';
 
-
 const ReviewForm = () => {
   const [selectedReason, handleRadioChange] = useRadioGroup('');
+  const [selectedRelax, handleRadioChangeSecond] = useRadioGroup('');
   const [rating, setRating] = useState(0);
 
   const { t } = useTranslation();
@@ -14,22 +14,18 @@ const ReviewForm = () => {
   const handleRatingChange = (e) => {
     setRating(parseInt(e.target.value));
   };
+    
 
   return (
     <form className="flex flex-col w-full h-full pb-10">
       <div className="flex flex-col w-full">
         <label className='review_label'>{t('review:question1')}</label>
         <div className="flex flex-col w-full">
-          <RadioButton value={t('review:answer1_1')}
-          onChange={handleRadioChange} />
-          <RadioButton value={t('review:answer1_2')}
-          onChange={handleRadioChange} />
-          <RadioButton value={t('review:answer1_3')}
-          onChange={handleRadioChange} />
-          <RadioButton value={t('review:answer1_4')}
-          onChange={handleRadioChange} />
-          <RadioButton value={t('review:another')}
-          onChange={handleRadioChange} />
+          <RadioButton value={t('review:answer1_1')} name="reason1" onChange={handleRadioChange} />
+          <RadioButton value={t('review:answer1_2')} name="reason1" onChange={handleRadioChange} />
+          <RadioButton value={t('review:answer1_3')} name="reason1" onChange={handleRadioChange} />
+          <RadioButton value={t('review:answer1_4')} name="reason1" onChange={handleRadioChange} />
+          <RadioButton value={t('review:another')} name="reason1" onChange={handleRadioChange} />
         </div>
         {selectedReason === 'Otro' && (
           <div className="flex flex-col w-full mt-4">
@@ -45,18 +41,13 @@ const ReviewForm = () => {
 
         <label className='review_label'>{t('review:question2')}</label>
         <div className="flex flex-col w-full">
-          <RadioButton value={t('review:answer2_1')}
-          onChange={handleRadioChange} />
-          <RadioButton value={t('review:answer2_2')}
-          onChange={handleRadioChange} />
-          <RadioButton value={t('review:answer2_3')}
-          onChange={handleRadioChange} />
-          <RadioButton value={t('review:answer2_4')}
-           onChange={handleRadioChange} />
-          <RadioButton value={t('review:another')}
-          onChange={handleRadioChange} />
+          <RadioButton value={t('review:answer2_1')} name="reason2" onChange={handleRadioChangeSecond} />
+          <RadioButton value={t('review:answer2_2')} name="reason2" onChange={handleRadioChangeSecond} />
+          <RadioButton value={t('review:answer2_3')} name="reason2" onChange={handleRadioChangeSecond} />
+          <RadioButton value={t('review:answer2_4')} name="reason2" onChange={handleRadioChangeSecond} />
+          <RadioButton value={t('review:another')} name="reason2" onChange={handleRadioChangeSecond} />
         </div>
-        {selectedReason === 'Otro' && (
+        {selectedRelax === 'Otro' && (
           <div className="flex flex-col w-full mt-4">
             <label htmlFor="otherRelax">{t('review:specify')}</label>
             <input
@@ -74,23 +65,22 @@ const ReviewForm = () => {
               key={starValue}
               star
               value={starValue}
+              name="rating"
               checked={rating >= starValue}
               onChange={handleRatingChange}
             />
           ))}
         </div>
-        <label className='review_label'>    
-            {t('review:question4')}      
-        </label>
-          <textarea
-            placeholder={t('review:specify')}
-            className="textarea textarea-bordered textarea-md w-full max-w-xs bg-white"></textarea>
-        <label className='review_label'>
-            {t('review:question5')}
-        </label>
+        <label className='review_label'>{t('review:question4')}</label>
         <textarea
           placeholder={t('review:specify')}
-          className="textarea textarea-bordered textarea-md w-full max-w-xs bg-white"></textarea>
+          className="textarea textarea-bordered textarea-md w-full bg-white"
+        ></textarea>
+        <label className='review_label'>{t('review:question5')}</label>
+        <textarea
+          placeholder={t('review:specify')}
+          className="textarea textarea-bordered textarea-md w-full  bg-white"
+        ></textarea>
       </div>
     </form>
   );
